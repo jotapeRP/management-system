4004b23801f5dbe2879547e96ec679fe076afe7f
João Paulo <jotaperp98@gmail.com>
2026-04-23 11:03:42 -0300

    Att backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const clientesRouter = require("./routes/clientes");
@@ -12,6 +12,8 @@ const servicosRouter = require("./routes/servicos");
 app.use("/clientes", clientesRouter);
 app.use("/servicos", servicosRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
