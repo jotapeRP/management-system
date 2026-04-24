4ffb91422db528a2ed046e58e91cf8a8edc8a634
João Paulo <jotaperp98@gmail.com>
2026-04-24 17:22:01 -0300

    fix database better-sqlite

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,8 @@ const Database = require("better-sqlite3");
 const db = new Database("database.db");
 
 db.serialize(() => {
-  db.run(`
+  db.prepare(
+    `
     CREATE TABLE IF NOT EXISTS clientes (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       nome TEXT,
@@ -16,9 +17,11 @@ db.serialize(() => {
       email TEXT,
       telefone TEXT
     )
-  `);
+  `,
+  ).run();
 
-  db.run(`
+  db.prepare(
+    `
   CREATE TABLE IF NOT EXISTS servicos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     cliente_id INTEGER,
@@ -29,7 +32,8 @@ db.serialize(() => {
     valor REAL,
     status TEXT
   )
-`);
+`,
+  ).run();
 });
 
 module.exports = db;
